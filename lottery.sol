9f212691c116dc0613f64e1c4e1e765460e64066
Rian Pollock <112873791+rpollock@users.noreply.github.com>
2024-01-15 17:27:21 +0000

    Update lottery.sol
    updated the smart contract, integrated chainlink

--- lottery.sol
+++ lottery.sol
@@ -7,9 +7,30 @@ contract Lottery {
     uint public lotteryId;
     mapping (uint => address payable) public lotteryHistory;
 
+    bytes32 internal keyHash; // identifies which Chainlink oracle to use
+    uint internal fee;        // fee to get random number
+    uint public randomResult;
+
     constructor() {
+        VRFConsumerBase(
+            0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B, // VRF coordinator
+            0x01BE23585060835E02B77ef475b0Cc51aA1e0709  // LINK token address
+        ) {
+            keyHash = 0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311;
+            fee = 0.1 * 10 ** 18;    // 0.1 LINK
+
         owner = msg.sender;
         lotteryId = 1;
+    }
+
+     function getRandomNumber() public returns (bytes32 requestId) {
+        require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK in contract");
+        return requestRandomness(keyHash, fee);
+    }
+
+    function fulfillRandomness(bytes32 requestId, uint randomness) internal override {
+        randomResult = randomness;
+        payWinner();
     }
 
     function getWinnerByLottery(uint lottery) public view returns (address payable) {
@@ -31,17 +52,18 @@ contract Lottery {
         players.push(payable(msg.sender));
     }
 
-    function getRandomNumber() public view returns (uint) {
-        return uint(keccak256(abi.encodePacked(owner, block.timestamp)));
+     function pickWinner() public onlyowner {
+        getRandomNumber();
     }
 
-    function pickWinner() public onlyOwner {
-        uint index = getRandomNumber() % players.length;
-        players[index.transfer(address(this).balance)]
+    function payWinner() public {
+        uint index = randomResult % players.length;
+        players[index].transfer(address(this).balance);
+
         lotteryHistory[lotteryId] = players[index];
         lotteryId++;
         
-        ///@dev resets the state of the contract
+        // reset the state of the contract
         players = new address payable[](0);
     }
 
